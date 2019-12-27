df4d81f4a1de4474f24aeab41425b88b9d7c7640
Any Name <kappa@Pride.twitch>
2019-12-27 09:51:08 +0300

    jfdvbkhfsgk

--- src/app/center/center.component.ts
+++ src/app/center/center.component.ts
@@ -10,22 +10,8 @@ export class CenterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    $(document).ready(function () {
-      $("#myInput").on("keyup", function () {
-        var value = $(this).val().toLowerCase();
-        if (value != 0) {
-          $("#myList div").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-          });
-        }
-        else {
-          $("#myList div").filter(function () {
-            $(this).toggle($(this).text().toLowerCase().indexOf(value) > 1)
-          });
-        }
-      }
-      );
-    });
+    
+
   }
 }
 
